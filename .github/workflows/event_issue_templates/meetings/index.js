63b1ce094f9c12f643633fa0ca265385ee23f1c0
derberg <lpgornicki@gmail.com>
2022-03-24 22:05:15 +0100

    log

--- .github/workflows/event_issue_templates/meetings/index.js
+++ .github/workflows/event_issue_templates/meetings/index.js
@@ -9,19 +9,21 @@ const { parseDate } = require('../utils/date.js');
  * @param {string} zoom Link to zoom meeting
 */
 module.exports = (date, time, core, getMeetingIssueContent, zoom) => {
-    
+    console.log('3')
     core.info(`Workflow triggered with the following hour ${time} and date ${date}`);
+    console.log('4')
     const dateDetails = parseDate(`${ date }T${ time }:00:00Z`);
-    
+    console.log('5')
     core.info('This is how time and date looks like after parsing:');
+    console.log('6')
     core.info(JSON.stringify(dateDetails));
-
+    console.log('7')
     if (dateDetails === 'Invalid Date') core.setFailed('Invalid date of the event. Make sure that you provided correct hour of the meeting and date in a format described in the meeting input form.')
-
+    console.log('8')
     const issueContent =  getMeetingIssueContent(dateDetails.hour, dateDetails.formattedDate, zoom);
-
+    console.log('9')
     writeFileSync('content.md', issueContent, { encoding: 'utf8'});
-
+    console.log('10')
     core.setOutput('formattedDate', dateDetails.formattedDate);
     core.setOutput('fullDate', dateDetails.fullDate);
     core.setOutput('hour', dateDetails.hour);
