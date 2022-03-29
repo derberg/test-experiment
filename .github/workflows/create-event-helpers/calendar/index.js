47b10a186d1f5c2b096a9dffa3632df46d0ed882
derberg <lpgornicki@gmail.com>
2022-03-29 10:35:25 +0200

    cleanup like a pro

--- .github/workflows/create-event-helpers/calendar/index.js
+++ .github/workflows/create-event-helpers/calendar/index.js
@@ -14,33 +14,47 @@ const calendar = google.calendar({ version: 'v3', auth });
  * Adds new single-occuring event
  * All events are being linked to their GitHub issues
  * @param {String} title Title of the event
+ * @param {String} suffix Suffix of the title of the event
  * @param {String} description Description of the event
  * @param {String} zoomUrl Zoom url of the meeting
  * @param {String} startDate ex. 2022-04-05
  * @param {String} startTime ex. 08 or 16
  * @param {Number} issueNumber GitHub issue number of the event, to find event later
  */
-async function addEvent(title, description, zoomUrl, startDate, startTime, issueNumber) {
+async function addEvent(title, suffix, description, zoomUrl, startDate, startTime, issueNumber) {
 
     try {
         const communityIssuesUrl = 'https://github.com/asyncapi/community/issues/';
-        const endTime = (startTime) => {
+
+        //helper to create end time which is always 1h later
+        const getEndTime = (startTime) => {
             const time = Number(startTime);
             if (time < 10) return '0' + (time + 1)
 
             return (time + 1) + ''
         }
 
+        //helper to build meeting description
+        //there is a use case that meeting has no connection over zoom available as it is pure live stream
+        const getDescription = (description, communityIssuesUrl, issueNumber, zoomUrl) => {
+
+            const zoomDetails = zoomUrl && `<b>Zoom</b>: <a href="${zoomUrl}">Meeting Link</a>`;
+            const agendaDetails = `<b>Agenda and more options to join the meeting</b>: <a href="${communityIssuesUrl}${issueNumber}">GitHub Issue Link.</a>`;
+
+            return `${ description }<br><br>${ zoomDetails }<br><br>${ agendaDetails }
+            `
+        };
+
         await calendar.events.insert({
             calendarId: process.env.CALENDAR_ID,
             requestBody: {
-                summary: title,
-                description: `${description}<br><b>Zoom</b>: <a href="${zoomUrl}">Meeting Link</a><br><b>Agenda</b>: <a href="${communityIssuesUrl}${issueNumber}">GitHub Issue Link.</a>`,
+                summary: title + suffix,
+                description: getDescription(description, communityIssuesUrl, issueNumber, zoomUrl),
                 start: {
                     dateTime: `${ startDate }T${ startTime }:00:00Z`
                 },
                 end: {
-                    dateTime: `${ startDate }T${ endTime(startTime) }:00:00Z`
+                    dateTime: `${ startDate }T${ getEndTime(startTime) }:00:00Z`
                 },
                 location: zoomUrl,
                 extendedProperties: {
