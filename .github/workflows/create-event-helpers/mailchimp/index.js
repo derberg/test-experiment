8058a2f112d292cebf43c401995ecff571dc21cf
derberg <lpgornicki@gmail.com>
2022-03-30 17:54:38 +0200

    test

--- .github/workflows/create-event-helpers/mailchimp/index.js
+++ .github/workflows/create-event-helpers/mailchimp/index.js
@@ -6,7 +6,7 @@ const htmlContent = require('./htmlContent.js');
  * @param {string} events List of events in a string in format "[{\"title\":\"Community Meeting \",\"issueId\":\"143\",\"date\":\"Tue, 05 Apr 2022 16:00:00 GMT\"},{\"title\":\"Let's talk about contributing - Website\",\"issueId\":\"144\",\"date\":\"Mon, 04 Apr 2022 18:00:00 GMT\"}]"
  */
 module.exports = async(events) => {
-    console.log('dupa', events)
+
     const parsedList = JSON.parse(events);
 
     if (!parsedList.length) return core.info('No events scheduled for next week so no email will be sent');
@@ -33,20 +33,19 @@ module.exports = async(events) => {
             }
         });
     } catch (error) {
-        core.setFailed(`Failed creating campaign: ${ JSON.stringify(error) }`)
+        core.setFailed(`Failed creating campaign: ${ JSON.stringify(error) }`);
     }
 
     try {
         await mailchimp.campaigns.setContent(newCampaign.id, { html: htmlContent(parsedList) });
     } catch (error) {
-        core.setFailed(`Failed adding content to campaign: ${ JSON.stringify(error) }`)
+        core.setFailed(`Failed adding content to campaign: ${ JSON.stringify(error) }`);
     }
 
     try {
         await mailchimp.campaigns.send(newCampaign.id);
-
     } catch (error) {
-        core.setFailed(`Failed sending email: ${ JSON.stringify(error) }`)
+        core.setFailed(`Failed sending email: ${ JSON.stringify(error) }`);
     }
 
     core.info(`New email campaign created`);
